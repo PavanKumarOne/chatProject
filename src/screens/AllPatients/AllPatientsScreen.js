2d35fc9591852f4a93a908f18b6c1356760c08e9
Pummy <Pummy@OneCares-MacBook-Pro-2.local>
2022-04-09 18:36:28 +0530

    Input Box added

--- src/screens/AllPatients/AllPatientsScreen.js
+++ src/screens/AllPatients/AllPatientsScreen.js
@@ -2,12 +2,19 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import {Heading} from '../../components/atoms/text/heading';
 import {Header} from '../../components/molecules/header';
+import {InputBox} from '../../components/atoms/input'
 
 export const AllPatientsScreen = () => {
+
+  const onChangeText=(data)=>{
+    // console.log(data);
+  }
+
   return (
     <View style={styles.container}>
       <Header onBackPress={() => {}} title={'All Patients'} />
       <Heading fontWeight="bold">AllPatientsScreen</Heading>
+      <InputBox placeholder="Search by patient name" onChangeText={onChangeText}/>
     </View>
   );
 };
