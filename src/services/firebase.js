dc0003d72646eab4a961a9cfb5d3717468cc928b
Shivam <Shivam@Ones-MacBook-Pro.local>
2022-04-10 08:14:06 +0530

    Shivam: added firebase configgs

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,28 @@
 import firestore from '@react-native-firebase/firestore';
+import firebase from '@react-native-firebase/app';
 import {StorageService, StorageKeys} from './storageService';
+
+const config = {
+  apiKey: 'AIzaSyCX0TTAKXeT2wmxblaZeMOumS6VYI9t8C0',
+  projectId: 'onecare-chat',
+  appId: '1:620472215963:android:2c869bdab447e688af593e',
+  databaseURL: 'https://homzhub-deeplinking.firebaseio.com/',
+  messagingSenderId: '894249713165',
+  authDomain: 'homzhub-deeplinking.firebaseapp.com',
+  storageBucket: 'homzhub-deeplinking.appspot.com',
+};
 
 class FirebaseService {
   deviceToken;
   fireStoreInstance;
 
   constructor() {
+    this.init();
     this.fireStoreInstance = firestore();
+  }
+
+  async init() {
+    await firebase.initializeApp(config);
   }
 
   getCollectionData(collectionName) {
